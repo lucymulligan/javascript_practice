2375a47696f2b59d61b0b1eb61de13232941e513
lucymulligan <lucymulligan@hotmail.com>
2019-05-20 17:12:19 -0700

    Shortened written tests

--- test/week9.test.js
+++ test/week9.test.js
@@ -2,19 +2,16 @@ const { sumMultiples } = require("../challenges/week9");
 
 describe("sumMultiples", () => {
     test("returns the sum of any numbers which are a multiple of 3 or 5", () => {
-        const result = sumMultiples([1, 3, 5]);
-        const expected = 8;
-        expect(result).toBe(expected);
+        expect(sumMultiples([1, 3, 5])).toBe(8);
 
     });
+
+    test("the test still works with decimal numbers", () => {
+        expect(sumMultiples([1.5, 3.5, 5, 5.2, 3, 2.47])).toBe(8);
+
+    });
+
+    test("returns zero if there are no multiples of 3 or 5", () => {
+        expect(sumMultiples([1, 7, 11, 4])).toBe(0);
+    });
 });
-
-
-
-
-
- //isValidDNA,
-    //getComplementaryDNA,
-    //isItPrime,
-    //createMatrix,
-    //areWeCovered
